309b3f14fe8c580d5f8c0dc3ada56a5843aabeb2
Andrey Karimov <AndreyKarimov@gmail.com>
2023-05-11 16:34:20 +0300

    done

---
+++ js/index.js
@@ -0,0 +1,55 @@
+const CURRENCY = 'руб.';
+const STATUS_IN_LIMIT = `Все хорошо`;
+const STATUS_OUT_OF_LIMIT = `Все плохо`;
+
+
+
+
+const inputNode = document.querySelector('.js-input__sum');
+const buttonNode = document.querySelector('.js-input__btn');
+const historyNode = document.querySelector('.js-history__list')
+const totalNode = document.querySelector('.js-total__value');
+const statusNode = document.querySelector('.js-status__value');
+const limitNode = document.querySelector('.js-limit__value');
+const RED_CLASS_NAME = 'red';
+
+const costs = [];
+const limit = 20000;
+let sum = 0;
+
+init();
+function init() {
+    limitNode.innerHTML = `${limit} ${CURRENCY}`;
+    statusNode.innerText = STATUS_IN_LIMIT;
+    totalNode.innerHTML = `${sum} ${CURRENCY}`
+}
+
+buttonNode.addEventListener('click', function() {
+    // append costs
+    if (!inputNode.value) {
+        return;
+    }
+    const transactionValue = parseInt(inputNode.value);
+    costs.push(transactionValue);
+    inputNode.value = '';
+
+    // history list output + count sum 
+    let elemListHTML = '';
+    
+
+    costs.forEach(elem => {
+        elemListHTML += `<li class="history__item">${elem} ${CURRENCY}</li> `;
+        sum += elem;
+        if (sum > limit) {
+            statusNode.innerHTML = STATUS_OUT_OF_LIMIT;
+            statusNode.classList.add(RED_CLASS_NAME);
+        } else {
+            statusNode.innerHTML = STATUS_IN_LIMIT;
+            statusNode.classList.remove(RED_CLASS_NAME);
+        }
+    })
+    
+    historyNode.innerHTML = elemListHTML;
+    totalNode.innerHTML = `${sum} ${CURRENCY}`;
+    
+})
